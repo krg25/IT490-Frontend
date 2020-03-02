256e57832e0c4a45eddba237e18fbc49a2c5a61e
Kenneth <krg25@njit.edu>
2020-03-02 12:54:01 -0500

    Updates for DB/DMZ functionality in the search

---
+++ search.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<?php include("header.php"); ?>
+<div class="content">
+<div class="container">
+
+
+<?php
+ini_set('display_errors',1);
+require_once('rabbit/path.inc');
+require_once('rabbit/get_host_info.inc');
+require_once('rabbit/rabbitMQLib.inc');
+
+
+if (isset($_GET["search"])){
+$client = new rabbitMQClient("rabbit/rabbit.ini","database");
+
+$request['type'] = "API";
+$request['symbol'] = $_GET["search"];
+
+$response = $client->send_request($request);
+
+switch($response['returnCode']){
+	case 0:
+		echo ("Server error, please retry");
+		break;
+	case 1:
+		echo ($response['symbol']);
+		/*echo ("
+		<header>
+		<h1>".$response['username']."--".$response['id']."</h1></br>
+		<p>".$response['fname']." ".$response['lname']."</p>
+		<p> Date Joined: ".$response['date']."</p>
+		</header>");*/
+		break;
+	case 2:
+		echo ("
+		<header>
+		<h1>Stock not found.</h1>
+		</header>
+		");
+		break;
+
+
+}//switch
+
+
+}else{
+echo ("
+<header>
+<h1>No stock specified!</h1>
+</header>
+");
+}
+
+
+?>
+</div>
+</div>
+</body>
+</html>
