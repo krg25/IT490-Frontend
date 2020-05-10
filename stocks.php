adaec01ab3c4645795bcd6df92d994839ac208e6
ponponderp <38149022+ponponderp@users.noreply.github.com>
2020-05-10 03:01:17 -0400

    RSS Feed

--- stocks.php
+++ stocks.php
@@ -1,21 +1,21 @@
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="utf-8">
-
-<link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 <body>
 <?php include("header.php"); ?>
-<div class="content">
-	<div class="container">
+    <!-- rss feed -->
+        <div class="feedgrabbr_widget" id="fgid_b53a2c64da5f39d3f5c0ab15a"></div>
+            <script>if (typeof (fg_widgets) === "undefined") fg_widgets = new Array(); fg_widgets.push("fgid_b53a2c64da5f39d3f5c0ab15a");</script>
+            <script async src="https://www.feedgrabbr.com/widget/fgwidget.js"></script>
+
 	</div>
-<header>
-    <h1>Stocks</h1>
-</header>
-<!-- preloaded market data should go here -->
-    
-</div>
 </body>
 </html>
